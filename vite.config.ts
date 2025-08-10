4095022e58a2883270238a34bc53fb8d4b4a4225
Hammad Ahmad <hamadqur447@gmail.com>
2025-08-10 20:13:54 +0500

    fix production issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import gzipPlugin from "rollup-plugin-gzip";
-
 import dotenv from "dotenv";
+
 dotenv.config();
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ important for Netlify
   build: {
     rollupOptions: {
-      plugins: [
-        gzipPlugin({
-          filter: /\.(js|css|html|json|svg)$/,
-          minSize: 1024,
-        }),
-      ],
+      // Any other rollup config you have goes here
     },
   },
 });
